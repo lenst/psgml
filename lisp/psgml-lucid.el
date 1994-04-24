b0bd7a4bdb4ca41e95687429bae5bcbf19330223
Lennart Staflin <lenst@lysator.liu.se>
1994-04-24 18:16:29 +0000

    Install Bill P. patch. Special Lucid face setting code. Fix use of new sgml-doctype-insert.

--- lisp/psgml-lucid.el
+++ lisp/psgml-lucid.el
@@ -31,7 +31,11 @@
 
 ;;;; Code:
 
-;;; These functions are needed for menus, etc. to work in lucid emacs
+(provide 'psgml-lucid)
+(require 'psgml)
+
+
+;;;; These functions are needed for menus, etc. to work in lucid emacs
 
 (defun sgml-fix-x-menu (menudesc)
   "Take a menu for x-popup-menu and return a lucid menu."
@@ -127,24 +131,80 @@
     ["Expand" sgml-expand-element t]
     ))
 
-(defvar sgml-sgml-menu
-  '("SGML"
-    ["Next data field"  sgml-next-data-field t]
-    ["End element" sgml-insert-end-tag t]
-    ["Show context" sgml-show-context t]
-    ["What element" sgml-what-element t]
-    ["Next trouble spot" sgml-next-trouble-spot t]
-    ["Edit attributes" sgml-edit-attributes t]
-    ["Change element name" sgml-change-element-name t]
-    ["Show valid tags" sgml-list-valid-tags t]
-    ["Show/hide warning log" sgml-show-or-clear-log t]
-    ["Validate" sgml-validate t]
-    ["Normalize" sgml-normalize t]
-    ["Fill element" sgml-fill-element t]
-    ["Options" sgml-options-menu t]
-    ["Save options" sgml-save-options t]
-    ["Submit bug report" sgml-submit-bug-report t]
-    ))
+(if (not (boundp 'emacs-major-version))
+    (defvar sgml-sgml-menu
+      '("SGML"
+      ["Next data field"  sgml-next-data-field t]
+      ["End element" sgml-insert-end-tag t]
+      ["Show context" sgml-show-context t]
+      ["What element" sgml-what-element t]
+      ["Next trouble spot" sgml-next-trouble-spot t]
+      ["Edit attributes" sgml-edit-attributes t]
+      ["Change element name" sgml-change-element-name t]
+      ["Show valid tags" sgml-list-valid-tags t]
+      ["Show/hide warning log" sgml-show-or-clear-log t]
+      ["Validate" sgml-validate t]
+      ["Normalize" sgml-normalize t]
+      ["Fill element" sgml-fill-element t]
+      ["Options" sgml-options-menu t]
+      ["Save options" sgml-save-options t]
+      ["Submit bug report" sgml-submit-bug-report t]
+      ))
+  (defvar sgml-sgml-menu
+    (list
+     "SGML"
+     ["Next data field"  sgml-next-data-field t]
+     ["End element" sgml-insert-end-tag t]
+     ["Show context" sgml-show-context t]
+     ["What element" sgml-what-element t]
+     ["Next trouble spot" sgml-next-trouble-spot t]
+     ["Edit attributes" sgml-edit-attributes t]
+     ["Change element name" sgml-change-element-name t]
+     ["Show valid tags" sgml-list-valid-tags t]
+     ["Show/hide warning log" sgml-show-or-clear-log t]
+     ["Validate" sgml-validate t]
+     ["Normalize" sgml-normalize t]
+     ["Fill element" sgml-fill-element t]
+     (cons "Options"
+         (mapcar
+          (function
+           (lambda (var)
+             (vector (capitalize
+                      (mapconcat (function (lambda (x)
+                                             (if (= x ?-) " "
+                                               (char-to-string x))))
+                                 (substring (symbol-name var) 5 nil)
+                                 ""))
+                     (list 'setq var (list 'not var))
+                     :style toggle :selected var)))
+          '(sgml-balanced-tag-edit
+            sgml-auto-insert-required-elements
+            sgml-omittag-transparent
+            sgml-leave-point-after-insert
+            sgml-indent-data
+            sgml-always-quote-attributes
+            sgml-warn-about-undefined-elements
+            sgml-normalize-trims
+            sgml-omittag
+            sgml-shorttag
+            sgml-debug)))
+     (cons "Indent Step"
+         (mapcar
+          (function
+           (lambda (entry)
+             (vector (car entry)
+                     (list 'setq 'sgml-indent-step (cdr entry))
+                     ':style 'radio ':active t
+                     ':selected
+                     (list 'eq 'sgml-indent-step (cdr entry)))))
+          '(("None" . nil)
+            ("0" . 0) ("1" . 1) ("2" . 2) ("3" . 3)
+            ("4" . 4) ("5" . 5) ("6" . 6) ("7" . 7) ("8" . 8))))
+     ["Save options" sgml-save-options t]
+     ["Submit bug report" sgml-submit-bug-report t]
+     )
+    )
+  )
 
 (defun sgml-build-custom-menus ()
   (and sgml-custom-markup (add-menu-item '("Markup") "------------" nil t))
@@ -157,18 +217,81 @@
   (mapcar (function
 	   (lambda (x)
 	     (add-menu-item '("DTD") (nth 0 x)
-			    (list 'sgml-doctype-insert
-				  (cadr x) (caddr x))
-			    
+			    (list 'apply ''sgml-doctype-insert
+				  (list 'quote (cdr x)))
 			    t)))
 	  sgml-custom-dtd))
 
 (define-key sgml-mode-map [button3] 'sgml-tags-menu)
 
 
+;;;; Insert with properties
+
+(defun sgml-insert (props format &rest args)
+  (let ((start (point)))
+    (insert (apply (function format)
+		   format
+		   args))
+    (when sgml-running-lucid		
+      (remf props 'rear-nonsticky))	; not useful in Lucid
+    (add-text-properties start (point) props)
+    ;; A read-only value of 1 is used for the text after values
+    ;; and this should in Lucid be open at the front.
+    (if (and sgml-running-lucid
+	     (eq 1 (getf props 'read-only)))
+      (set-extent-property
+       (extent-at start nil 'read-only)
+       'start-open t))))
+
+
+;;;; Set face of markup
+
+(defun sgml-set-face-for (start end type)
+  (let ((face (cdr (assq type sgml-markup-faces)))
+	o)
+    (loop for e being the extents from start to end
+	  do (when (extent-property e 'type)
+	       (cond ((and (null o)
+			   (eq type (extent-property e 'type)))
+		      (setq o e))
+		     (t (delete-extent e)))))
+
+    (cond (o
+	   (set-extent-endpoints o start end))
+	  (face
+	   (setq o (make-extent start end))
+	   (set-extent-property o 'type type)
+	   (set-extent-property o 'face face)
+	   (set-extent-face o face)))))
+
+(defun sgml-set-face-after-change (start end &optional pre-len)
+  (when sgml-set-face
+    (let ((o (extent-at start nil 'type)))
+      (cond
+       ((null o))
+       ((= start (extent-start-position o))
+	(set-extent-endpoints o end (extent-end-position o)))
+       (t (delete-extent o))))))
+
+;(defalias 'next-overlay-at 'next-overlay-change) ; fix bug in cl.el
+
+(defun sgml-clear-faces ()
+  (interactive)
+  (loop for o being the overlays
+	if (extent-property o 'type)
+	do (delete-extent o)))
+
+
+;;;; Functions not in Lucid Emacs
+
+(unless (fboundp 'frame-width)
+  (defalias 'frame-width 'screen-width))
+
+
+
 ;;;; Provide
 
-(provide 'psgml-lucid)
+
 
 
 ;;; psgml-lucid.el ends here
